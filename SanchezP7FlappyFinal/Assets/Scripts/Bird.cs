2fd7056dd7d2dc04949ba2863940dd5bf42d5c69
752071-tech <752071@student.dusd.net>
2025-12-09 22:27:42 -0800

    I started animating my bird
    I started animating my bird

--- SanchezP7FlappyFinal/Assets/Scripts/Bird.cs
+++ SanchezP7FlappyFinal/Assets/Scripts/Bird.cs
@@ -8,22 +8,28 @@ public class Bird : MonoBehaviour
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (isDead = false)
+       if (isDead == false)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 rb2d.velocity = Vector2.zero;
-                rb2d.AddForce(new Vector2(0, upForce)); 
+                rb2d.AddForce(new Vector2 (0, upForce)); 
             }
         }
+    }
+
+    void OnCollisionEnter2D ()
+    {
+        isDead = true;
     }
 }
